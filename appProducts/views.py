bc0ad06fc3c21eda4b8d8fcd220f0af396e198ce
login <nikitooo111333222@gmail.com>
2024-05-05 23:45:59 +0300

    first

---
+++ appProducts/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, HttpResponse
+from appProducts.models import Product, ProductCategory
+
+# Create your views here.
+
+def index(request):
+    context = {
+        'title': 'Store',
+    }
+    return render(request, 'appProducts/index.html', context)
+
+
+def products(request):
+    context = {
+        'title': 'Store - Каталог',
+        'products': Product.objects.all(),
+        'category': ProductCategory.objects.all(),
+    }
+    return render(request, 'appProducts/products.html', context)
